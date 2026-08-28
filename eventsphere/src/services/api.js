70d0de04995bd4eb7287640ba1fa1eeebcd98fbf
= <=>
2026-08-28 11:10:37 +0500

    Integrate backend URL

--- eventsphere/src/services/api.js
+++ eventsphere/src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+  import.meta.env.VITE_API_URL || "https://eventspherebackend-d2gv.onrender.com";
 
 const apiRequest = async (endpoint, options = {}) => {
   const token = localStorage.getItem("token");
